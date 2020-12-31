baaaf012ae2e3de2fe1ee10932a40de54998597b
Andy <andy@cobrowse.io>
2020-12-31 15:53:32 +0000

    session list updates

--- src/Session.js
+++ src/Session.js
@@ -10,12 +10,14 @@ export default function Session(props) {
 
     return (
         <div style={props.style} className={`Session ${props.className||''}`}>
-            <UserIcon user={props.session.agent} />
+            <UserIcon className={'agent'} user={props.session.agent} />
             <div className={'details'}>
                 <div>Connected to <PlatformIcon platform={props.session.device.platform}/> <b>{deviceType(props.session.device)}</b></div>
-                <div className={'activated'}>{ moment(props.session.activated).fromNow() }</div>
+                <div className={'subdetails'}>
+                    <div className={'activated'}>{ moment(props.session.activated).fromNow() }</div>
+                    { props.session.state === 'ended' && props.session.recorded ? <div className={'recorded'}>Recorded</div> : null }
+                </div>
             </div>
-            { props.session.state === 'ended' && props.session.recorded ? <div className={'recorded'}>Recorded</div> : null }
             { props.session.state === 'ended' ?
                 <Stopwatch className={'duration'} start={props.session.activated} end={props.session.ended || new Date()} /> :
                 <div className={'active'}>Active</div>
