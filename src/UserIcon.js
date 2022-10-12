072c2037b9655030c6379060ddc19c0eaa828d54
Dani <dyon@users.noreply.github.com>
2022-10-12 13:22:09 +0200

    style: apply StandardJS (#6)

--- src/UserIcon.js
+++ src/UserIcon.js
@@ -1,29 +1,28 @@
-import React from 'react';
+import React from 'react'
 
-import './UserIcon.css';
+import './UserIcon.css'
 
-export default function UserIcon(props) {
+export default function UserIcon (props) {
+  function initials () {
+    if (!props.user) return ''
+    if (!props.user.name) return ''
+    if (props.user.picture) return ''
+    return props.user.name.split(' ')
+      .map(i => i.toUpperCase().charAt(0))
+      .join('').substr(0, 2)
+  }
 
-    function initials() {
-        if (!props.user) return '';
-        if (!props.user.name) return '';
-        if (props.user.picture) return '';
-        return props.user.name.split(' ')
-            .map(i => i.toUpperCase().charAt(0))
-            .join('').substr(0,2);
-    }
-
-    if (!props.user) return null;
-    return (
-        <div
-            className={`UserIcon ${props.className||''}`}
-            title={props.user.name||null}
-            style={{
-                backgroundImage: props.user.picture ? `url(${props.user.picture||''})` : undefined,
-                backgroundColor: props.user.colour
-            }}
-        >
-            <span>{initials()}</span>
-        </div>
-    );
+  if (!props.user) return null
+  return (
+    <div
+      className={`UserIcon ${props.className || ''}`}
+      title={props.user.name || null}
+      style={{
+        backgroundImage: props.user.picture ? `url(${props.user.picture || ''})` : undefined,
+        backgroundColor: props.user.colour
+      }}
+    >
+      <span>{initials()}</span>
+    </div>
+  )
 }
