0c03d1754ff9b7128634eccb89e56f3487821361
Dani <hello@dyon.me>
2025-11-24 13:33:36 +0100

    chore: extract last seen to its own component in Device

--- lib/components/Device/index.jsx
+++ lib/components/Device/index.jsx
@@ -4,41 +4,35 @@ import PlatformIcon from '../PlatformIcon'
 import deviceType from '../../deviceType.js'
 import './Device.css'
 
-const Device = ({ style, device, children }) => {
-  const renderLastSeen = () => {
-    if (device.online) {
-      return <div className='last-seen'>{i18n.t('Online')}</div>
-    } else {
-      return (
-        <div className='last-seen'>
-          {i18n.t('Last seen {{date, dateRelative}}', {
-            date: new Date(device.last_active)
-          })}
-        </div>
-      )
-    }
-  }
+const LastSeen = ({ device }) => (
+  <div className='last-seen'>
+    {device.online
+      ? i18n.t('Online')
+      : i18n.t('Last seen {{date, dateRelative}}', {
+        date: new Date(device.last_active)
+      })}
+  </div>
+)
 
-  return (
-    <div
-      style={style}
-      className={`Device ${device.online ? 'online' : ''} ${
-        device.connectable ? 'connectable' : ''
-      }`}
-    >
-      <div className='device-status'>
-        <PlatformIcon
-          platform={device.device.platform}
-          className='platform-icon'
-        />
-      </div>
-      <div className='details'>
-        {deviceType(device.device)}
-        {renderLastSeen()}
-      </div>
-      {children}
+const Device = ({ style, device, children }) => (
+  <div
+    style={style}
+    className={`Device ${device.online ? 'online' : ''} ${
+      device.connectable ? 'connectable' : ''
+    }`}
+  >
+    <div className='device-status'>
+      <PlatformIcon
+        platform={device.device.platform}
+        className='platform-icon'
+      />
     </div>
-  )
-}
+    <div className='details'>
+      {deviceType(device.device)}
+      <LastSeen device={device} />
+    </div>
+    {children}
+  </div>
+)
 
 export default Device
