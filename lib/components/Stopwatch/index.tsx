1e9b8c8a8fcf8117a1e4f61e857aec3897c49f4f
Dani <hello@dyon.me>
2025-12-02 11:46:24 +0100

    feat: convert Stopwatch

---
+++ lib/components/Stopwatch/index.tsx
@@ -0,0 +1,26 @@
+import clsx from 'clsx'
+import { differenceInMilliseconds, intervalToDuration, format } from 'date-fns'
+import { type CSSProperties } from 'react'
+
+export interface StopwatchProps {
+  start?: Date | number | string | null
+  end?: Date | number | string | null
+  style?: CSSProperties
+  className?: string
+}
+
+const Stopwatch = ({ start: startProp, end: endProp, style, className }: StopwatchProps) => {
+  const start = startProp ? new Date(startProp) : new Date(0)
+  const end = endProp ? new Date(endProp) : new Date()
+  const ms = differenceInMilliseconds(end, start)
+  const duration = intervalToDuration({ start, end })
+
+  return (
+    <span style={style} className={clsx('Stopwatch', className)}>
+      {duration.hours && duration.hours > 0 ? <span>{duration.hours}<span>:</span></span> : null}
+      <span>{format(new Date(ms), 'mm:ss')}</span>
+    </span>
+  )
+}
+
+export default Stopwatch
