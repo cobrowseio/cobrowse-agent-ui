af2fa2f3499be089ec40601ce1b8b7d91de3d655
Dani <hello@dyon.me>
2025-12-03 13:56:34 +0100

    feat: export more types

--- lib/main.ts
+++ lib/main.ts
@@ -1,18 +1,19 @@
 import CodeEntryComponent from './components/CodeEntry'
 import PlatformIconComponent from './components/PlatformIcon'
 import DeviceComponent from './components/Device'
-import SmartConnectButtonComponent from './components/SmartConnectButton'
 import SessionComponent from './components/Session'
+import SmartConnectButtonComponent from './components/SmartConnectButton'
 import UserIconComponent from './components/UserIcon'
 import i18nInstance from './i18n'
 
-export type { DeviceData } from './components/Device'
-export type { SessionData } from './components/Session'
+export type { DeviceData, DeviceProps } from './components/Device'
+export type { SessionData, SessionProps } from './components/Session'
+export type { SmartConnectButtonProps } from './components/SmartConnectButton'
 
 export const CodeEntry = CodeEntryComponent
 export const PlatformIcon = PlatformIconComponent
 export const Device = DeviceComponent
-export const SmartConnectButton = SmartConnectButtonComponent
 export const Session = SessionComponent
+export const SmartConnectButton = SmartConnectButtonComponent
 export const UserIcon = UserIconComponent
 export const i18n = i18nInstance
