3683400972f1c6d218d89d0e5f0391e5750dc3f6
Dani <hello@dyon.me>
2025-12-11 11:31:35 +0100

    feat: add base styling to Tabs

--- lib/components/Tabs/index.tsx
+++ lib/components/Tabs/index.tsx
@@ -1,3 +1,4 @@
+import clsx from 'clsx'
 import type { MouseEventHandler, ReactNode } from 'react'
 import {
   Tab as HeadlessTab,
@@ -12,6 +13,7 @@ import SessionsPanel from '@/components/Tabs/SessionsPanel'
 import type { DeviceData } from '@/components/Device'
 import type { SessionData } from '@/components/Session'
 import RefreshIcon from '@/icons/refresh.svg?react'
+import styles from './Tabs.module.css'
 
 export interface TabsProps {
   devices: DeviceData[] | null
@@ -20,25 +22,78 @@ export interface TabsProps {
   onRefreshClick?: MouseEventHandler
   smartConnectButtonClassName?: string
   loader?: ReactNode
+  className?: string
+  headerClassName?: string
+  tabListClassName?: string
+  tabClassName?: string
+  tabHoverClassName?: string
+  tabActiveClassName?: string
+  tabPanelsClassName?: string
+  refreshButtonClassName?: string
 }
 
-const Tabs = ({ devices, sessions, onSessionClick, onRefreshClick, smartConnectButtonClassName, loader }: TabsProps) => {
+const Tabs = ({
+  devices,
+  sessions,
+  onSessionClick,
+  onRefreshClick,
+  smartConnectButtonClassName,
+  loader,
+  className,
+  headerClassName,
+  tabListClassName,
+  tabClassName,
+  tabHoverClassName,
+  tabActiveClassName,
+  tabPanelsClassName,
+  refreshButtonClassName
+}: TabsProps) => {
   const { t } = useTranslation()
 
   return (
-    <HeadlessTabGroup>
-      {onRefreshClick && (
-        <div>
-          <button onClick={onRefreshClick}>
+    <HeadlessTabGroup className={clsx(styles.root, className)}>
+      <div className={clsx(styles.header, headerClassName)}>
+        <HeadlessTabList className={clsx(styles.tabList, tabListClassName)}>
+          <HeadlessTab
+            className={({ selected, hover }) =>
+              clsx(
+                styles.tab,
+                tabClassName,
+                hover && styles.tabHover,
+                hover && tabHoverClassName,
+                selected && styles.tabActive,
+                selected && tabActiveClassName
+              )
+            }
+          >
+            {t('Devices')}
+          </HeadlessTab>
+          <HeadlessTab
+            className={({ selected, hover }) =>
+              clsx(
+                styles.tab,
+                tabClassName,
+                hover && styles.tabHover,
+                hover && tabHoverClassName,
+                selected && styles.tabActive,
+                selected && tabActiveClassName
+              )
+            }
+          >
+            {t('Sessions')}
+          </HeadlessTab>
+        </HeadlessTabList>
+        {onRefreshClick && (
+          <button
+            type='button'
+            className={clsx(styles.refreshButton, refreshButtonClassName)}
+            onClick={onRefreshClick}
+          >
             <RefreshIcon />
           </button>
-        </div>
-      )}
-      <HeadlessTabList>
-        <HeadlessTab>{t('Devices')}</HeadlessTab>
-        <HeadlessTab>{t('Sessions')}</HeadlessTab>
-      </HeadlessTabList>
-      <HeadlessTabPanels>
+        )}
+      </div>
+      <HeadlessTabPanels className={tabPanelsClassName}>
         <HeadlessTabPanel>
           <DevicesPanel devices={devices} smartConnectButtonClassName={smartConnectButtonClassName} loader={loader} />
         </HeadlessTabPanel>
