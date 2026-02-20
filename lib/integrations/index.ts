5147b2566ebf553da3a14d1475adc31532917e42
Dani <hello@dyon.me>
2026-02-20 11:05:44 +0100

    feat: add integrations CodeEntry component
    This uses the base CodeEntry component under the hood with
    integration-specific additions.

--- lib/integrations/index.ts
+++ lib/integrations/index.ts
@@ -1,2 +1,3 @@
 export { default as Tabs, type TabsProps } from './components/Tabs'
 export { default as BlockingError, type BlockingErrorProps } from './components/BlockingError'
+export { default as CodeEntry, type CodeEntryProps } from './components/CodeEntry'
