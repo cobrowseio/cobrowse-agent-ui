e4993911e636efd8cc6879faeb4f502675d3831a
Dani <hello@dyon.me>
2026-03-30 13:39:16 +0200

    feat: make Button polymorphic

--- lib/components/Button/index.tsx
+++ lib/components/Button/index.tsx
@@ -1,15 +1,36 @@
 import clsx from 'clsx'
-import type { ComponentPropsWithoutRef } from 'react'
+import type { ComponentPropsWithoutRef, ElementType } from 'react'
 import styles from './Button.module.css'
 
-export interface ButtonProps extends ComponentPropsWithoutRef<'button'> {}
+const DEFAULT_TAG = 'button' as const
 
-const Button = ({ type = 'button', className, ...props }: ButtonProps) => (
-  <button
-    type={type}
-    className={clsx(styles.root, className)}
-    {...props}
-  />
-)
+type PropsOf<T extends ElementType> = ComponentPropsWithoutRef<T>
+
+interface BaseButtonProps {
+  className?: string
+}
+
+export type ButtonProps<TElement extends ElementType = typeof DEFAULT_TAG> =
+  BaseButtonProps &
+  PropsOf<TElement> & {
+    as?: TElement
+  }
+
+const Button = <TElement extends ElementType = typeof DEFAULT_TAG>({
+  as,
+  className,
+  ...props
+}: ButtonProps<TElement>) => {
+  const Tag: ElementType = as ?? DEFAULT_TAG
+  const tagProps = props as PropsOf<ElementType> & { type?: string }
+
+  return (
+    <Tag
+      type={Tag === DEFAULT_TAG ? 'button' : undefined}
+      className={clsx(styles.root, className)}
+      {...tagProps}
+    />
+  )
+}
 
 export default Button
