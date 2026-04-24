4f59844a86893927b90012bad2e825f7b109cdf7
Simon Gaeremynck <gaeremyncks@gmail.com>
2026-04-24 17:11:23 +0100

    feat: add optional heading to BlockingError

--- lib/components/BlockingError/index.tsx
+++ lib/components/BlockingError/index.tsx
@@ -3,16 +3,17 @@ import { useTranslation } from '@/i18n'
 import styles from './BlockingError.module.css'
 
 export interface BlockingErrorProps {
+  heading?: string
   message?: string
   children?: ReactNode
 }
 
-const BlockingError = ({ message, children }: BlockingErrorProps) => {
+const BlockingError = ({ heading, message, children }: BlockingErrorProps) => {
   const { t } = useTranslation()
 
   return (
     <div className={styles.root}>
-      <p className={styles.heading}>{t('Sorry, something went wrong.')}</p>
+      <p className={styles.heading}>{heading ?? t('Sorry, something went wrong.')}</p>
       {message && <p className={styles.message}>{message}</p>}
       {children}
     </div>
